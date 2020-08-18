04addfb81441ead61ea06d7460cb50ab32deea41
Michal 'vorner' Vaner <vorner@vorner.cz>
2020-08-18 20:09:29 +0200

    Tests for locking writes

--- tests/locking.rs
+++ tests/locking.rs
@@ -1,9 +1,10 @@
 //! Test the feature that most operations (like `read_exact`) are not interruptible by a reopen.
 
-use std::io::{Error, ErrorKind, Read};
+use std::io::{Error, ErrorKind, Read, Write};
 use std::iter;
+use std::sync::{Arc, Mutex};
 
-use partial_io::{PartialOp, PartialRead};
+use partial_io::{PartialOp, PartialRead, PartialWrite};
 use reopen::{Handle, Reopen};
 
 /// Request reopen after each operation. That way we can check the reopen doesn't happen in the
@@ -16,6 +17,17 @@ struct RequestReopen<FD> {
 impl<FD: Read> Read for RequestReopen<FD> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize, Error> {
         let result = self.fd.read(buf);
+        self.handle.reopen();
+        result
+    }
+}
+
+impl<FD: Write> Write for RequestReopen<FD> {
+    fn flush(&mut self) -> Result<(), Error> {
+        self.fd.flush()
+    }
+    fn write(&mut self, buf: &[u8]) -> Result<usize, Error> {
+        let result = self.fd.write(buf);
         self.handle.reopen();
         result
     }
@@ -107,4 +119,84 @@ fn read_to_end() {
     // Reopens after the use, we can read another input.
     reader.read_to_end(&mut buf).unwrap();
     assert_eq!(b"hellohello", &buf[..]);
+}
+
+// Note: we are single threaded, but something wants Send, so we just comply.
+type History = Arc<Mutex<Vec<Vec<u8>>>>;
+
+struct HistoryWriter {
+    /// History of all the previous writers. Oldest first.
+    history: History,
+}
+
+impl Write for HistoryWriter {
+    fn flush(&mut self) -> Result<(), Error> {
+        self.history.lock().unwrap().last_mut().unwrap().flush()
+    }
+    fn write(&mut self, buf: &[u8]) -> Result<usize, Error> {
+        self.history.lock().unwrap().last_mut().unwrap().write(buf)
+    }
+}
+
+fn provide_writer() -> (Reopen<RequestReopen<PartialWrite<HistoryWriter>>>, History) {
+    let history = History::default();
+
+    let handle = Handle::stub();
+    let reopen = Reopen::with_handle(
+        handle.clone(),
+        Box::new({
+            let history = Arc::clone(&history);
+            move || {
+                let history = Arc::clone(&history);
+                history.lock().unwrap().push(Vec::new());
+                let writer = HistoryWriter { history };
+                let partial = PartialWrite::new(writer, iter::repeat(PartialOp::Limited(1)));
+                Ok(RequestReopen {
+                    fd: partial,
+                    handle: handle.clone(),
+                })
+            }
+        }),
+    )
+    .unwrap();
+
+    (reopen, history)
+}
+
+#[test]
+fn write_sanity_check() {
+    let (mut writer, history) = provide_writer();
+    assert_eq!(1, writer.write(b"hello").unwrap());
+    assert_eq!(1, writer.write(b"hello").unwrap());
+    assert_eq!(2, history.lock().unwrap().len());
+}
+
+#[test]
+fn write_all() {
+    let (mut writer, history) = provide_writer();
+    writer.write_all(b"hello").unwrap();
+    let history = history.lock().unwrap();
+    assert_eq!(1, history.len());
+    assert_eq!(b"hello", &history[0][..]);
+}
+
+#[test]
+fn write_explicit() {
+    let (mut writer, history) = provide_writer();
+    let lock = writer.lock().unwrap();
+    assert_eq!(1, lock.write(b"a").unwrap());
+    assert_eq!(1, lock.write(b"b").unwrap());
+    let history = history.lock().unwrap();
+    assert_eq!(1, history.len());
+    assert_eq!(b"ab", &history[0][..]);
+}
+
+#[test]
+#[allow(clippy::write_literal)] // The argument is useless, but we want to test it.
+fn format() {
+    let (mut writer, history) = provide_writer();
+    write!(&mut writer, "Hello {}", 42).unwrap();
+    let history = history.lock().unwrap();
+    assert_eq!(1, history.len());
+    assert_eq!(b"Hello 42", &history[0][..]);
 }
